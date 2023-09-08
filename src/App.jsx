df370044545fc174053969a31f3c4d74ca1e553a
alinadir44 <56518209+alinadir44@users.noreply.github.com>
2023-09-08 20:09:26 +0000

    updated app props object

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Ali Nadir",
+  title: "Cloud practicioner and Web Developer ",
+  email: "ali.nadir@studentambassadors.com",
+  gitHub: "github.com/alinadir44",
+  instagram: "kazmi.san",
+  linkedIn: "www.linkedin.com/in/salinadir",
+  medium: "@edgecake88",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
